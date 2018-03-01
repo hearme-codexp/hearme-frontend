694773cf6e28535376090b0ab90101720095b512
Bruno Alencar <bruno.idealencar@gmail.com>
2018-03-01 18:35:33 -0300

    Clean code and change color on leaked button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,13 +1,6 @@
 import React from 'react'
 import './button.css'
 
-// class Button extends React.Component {
-
-//     render(){
-//         return <button {...props}>{children}</button>
-//     }
-// }
-
 export default ({...props, children}) => {
     return <button {...props}>{children}</button>
 }
