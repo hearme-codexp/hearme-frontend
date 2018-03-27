8e6e98520d4c5638abdde92f366ad59f6439c05e
Bruno Alencar <bruno.idealencar@gmail.com>
2018-03-26 21:30:20 -0300

    Fix logo folder

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import './App.css';
 import RegisterForm from './components/RegisterForm'
 import Login from './components/login'
-import logo from './logo-hearme-02.png'
+import logo from '../src/img/logo-hearme-02.png'
 import Home from './components/page/home'
 
 class App extends Component {
@@ -15,8 +15,6 @@ class App extends Component {
             <img src={logo} alt="logo" width="150" className="logo" />
           </header>
           <div className="section">
-            {/*<RegisterForm options={["Leve", "Alta"]}/>*/}
-            {/* <Login />  */}
             <Route exact path="/login" component={Login} />
             <Route path="/cadastro" component={RegisterForm} />
             <Route path="/home" component={Home} />
