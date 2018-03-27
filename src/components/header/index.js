b047a4466775e49dd618380049929398698038f2
Bruno Alencar <bruno.idealencar@gmail.com>
2018-03-27 13:55:51 -0300

    Refactor on header component

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,15 +1,40 @@
 import React from "react"
 import './header.css';
 import logo from './logo-hearme-02.png'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-class header extends Component {
+class Header extends React.Component {
     render() {
         return (
-            <React.Fragment>
-                <header className="header">
-                    <img src={logo} alt="logo" width="150" className="logo" />
+                <header>
+                    <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+                    <img src={logo} className="hearme"/>
+                    <a className="navbar-brand" href="#">  </a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarCollapse">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item active">
+                                <Link className="nav-link" to="/landing">Home<span className="sr-only">(current)</span></Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/login">Login</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/cadastro">Cadastro</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/home">Home</Link>
+                            </li>
+                        </ul>
+                        <form className="form-inline mt-2 mt-md-0">
+                            <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
+                            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Sign In</button>
+                        </form>
+                    </div>
+                    </nav>
                 </header>
-            </React.Fragment>
         );
     }
 }
