d146cf8b087d8dc424448866b7fc206fd6cd2826
rosaju <rosajuuu@gmail.com>
2018-04-02 21:53:37 -0300

    atualização do css

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -8,9 +8,9 @@ class Header extends React.Component {
         return (
                 <header>
                     <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-                    <Link to="/"><img src={logo} className="hearme"/></Link>
+                    <Link to="/" className="home-button"><img src={logo} className="hearme"/></Link>
                     {/* <img src={logo} className="hearme"/> */}
-                    <a className="navbar-brand" href="#">  </a>
+                    {/* <a className="navbar-brand" href="#">  </a> */}
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
