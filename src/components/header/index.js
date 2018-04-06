a4de88b271097339675066e03abb5c44f75b63fa
Bruno Alencar <bruno.idealencar@gmail.com>
2018-04-06 22:26:57 +0000

    Remove jquery import

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,7 +3,6 @@ import classnames from 'classnames'
 import './header.css';
 import logo from './logo-hearme-02.png'
 import { withRouter, BrowserRouter as Router, Route, Link } from "react-router-dom";
-import $ from 'jquery'
 
 class Header extends React.Component {
     componentWillReceiveProps(nextProps) {
