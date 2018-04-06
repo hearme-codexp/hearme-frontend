3581b251a85604babf82502bd1f6049c4f1d4961
Bruno Alencar <bruno.idealencar@gmail.com>
2018-04-06 20:27:44 -0300

    Solve google maps api

--- src/components/maps/index.js
+++ src/components/maps/index.js
@@ -11,7 +11,7 @@ const demoFancyMapStyles = require("./demoFancyMapStyles.json");
 
 const MapWithAMarkerClusterer = compose(
   withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places",
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyCS8KIWpVjNHMryUXYXKY4nZZnfFoO8nlY&v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `400px` }} />,
     mapElement: <div style={{ height: `100%` }} />,
