afcb780a912b2236a3dec89d73de041564f4ba2c
Bruno Alencar <bruno.idealencar@gmail.com>
2018-04-06 21:38:32 -0300

    Add partners on footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -2,22 +2,39 @@ import React from 'react'
 import Contact from '../contact'
 import './footer.css'
 import Header from '../header'
+import senai from './senai.png'
+import jpMorgan from './jpmorgan.png'
+import microsoft from './microsoft.png'
 
 class Footer extends React.Component {
 
     render() {
         return (
             <React.Fragment>
-                <main className="section">
-                    <div>
-                        <div className="container">
+                <div className="item">
+                    <div className="container">
                             <Contact />
-                            <div className="container--copyright">
+                    </div>
+                   <div className="footer-companies">
+                        <div className="container">
+                            <h2 className="footer-companies__title">Partners:</h2>
+                            <div class="footer-companies__items">
+                                <div>
+                                    <img src={senai} className="footer-companies__partner" />
+                                </div> 
+                                <div>
+                                    <img src={jpMorgan}  className="footer-companies__partner" />
+                                </div>    
+                                <div>
+                                    <img src={microsoft}  className="footer-companies__partner" />
+                                </div>       
+                            </div>
+                            <div className="footer--copyright">
                                 <p>Copyright &copy; 2018 Hear me. All Rights Reserved. </p>
                             </div>
                         </div>
                     </div>
-                </main>
+                </div>
             </React.Fragment>
         );
     }
